6d860d568c9810bd4dc01cfa2d71112daea2ac54
Andrew Christ <andrew@openmarmot.com>
2026-02-01 18:10:51 -0700

    update

--- nvidia/DGX_Spark/llamacpp/start_glm_4.7_flash.sh
+++ nvidia/DGX_Spark/llamacpp/start_glm_4.7_flash.sh
@@ -1,5 +1,25 @@
-# designed for the dgx spark 
+#!/bin/bash
+
+# Designed for DGX Spark + GLM-4.7-Flash
 # https://unsloth.ai/docs/models/glm-4.7-flash
 # https://huggingface.co/unsloth/GLM-4.7-Flash-GGUF
+# https://huggingface.co/ggerganov/presets/blob/main/preset.ini
 
-./llama.cpp/build-cuda/bin/llama-server -hf unsloth/GLM-4.7-Flash-GGUF:UD-Q8_K_XL --ctx-size 0 --jinja --temp 1.0 --top-p 0.95 --min-p 0.01 --fit on --host 0.0.0.0 --port 8080
+# todo - implement this ?? 
+# https://github.com/ggml-org/llama.cpp/pull/19164
+
+# currently running at ~ 42 t/s on DGX Spark 
+
+./llama.cpp/build-cuda/bin/llama-server \
+  -hf ggml-org/GLM-4.7-Flash-GGUF:Q8_0 \
+  --ctx-size 0 \
+  --jinja \
+  --temp 1.0 \
+  --top-p 0.95 \
+  --top-k 0 \
+  --min-p 0.01 \
+  --batch-size 4096 \
+  --ubatch-size 4096 \
+  --host 0.0.0.0 \
+  --port 8080
+
