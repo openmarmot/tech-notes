185d6ca4dbe32acb1338b849cc885421a35fac4a
Andrew Christ <andrew@openmarmot.com>
2026-04-12 12:54:27 -0700

    update

--- nvidia/DGX_Spark/llamacpp/model_scripts/old_or_not_good_for_one_spark/start_minimax_m2.7.sh
+++ nvidia/DGX_Spark/llamacpp/model_scripts/old_or_not_good_for_one_spark/start_minimax_m2.7.sh
@@ -1,10 +1,18 @@
 
+# !!! Note that this is likely to exceeed your memory for 1x spark
+# probably best to try a q3
+# however I don't think this is a good model for one spark so I'm not developing it further.
+
+
 # minimax m 2.7
 #  note (on release) the license prohibits commercial use
 
 # https://unsloth.ai/docs/models/minimax-m27
 # https://huggingface.co/unsloth/MiniMax-M2.7-GGUF
 
+# !! uses 121gb of ram at 50k context. basically the absolute max.
+# you may need to trim context or go to a smaller quant
+# ~ 14 t/s
 HF_MODEL="unsloth/MiniMax-M2.7-GGUF:UD-IQ4_XS"
 
 ./llama.cpp/build-cuda/bin/llama-server \
